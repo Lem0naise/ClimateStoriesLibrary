b10117d515c42eb85fffb858d899bd5273859bc2
Zac Nolan <zac1i1nolan@gmail.com>
2024-07-20 20:57:06 +0100

    Functional everything

--- site/script.js
+++ site/script.js
@@ -7,6 +7,8 @@ window.onload = function(){
             document.getElementById("d" + this.id.slice(1)).style.display = 'flex';
         };
     }
+
+    loadMap();
 }
 
 
@@ -17,4 +19,22 @@ document.onclick = function(){
             menuDrops[i].style.display = 'none';
         };
     }
+}
+
+function loadMap(){ // loading from leaflet js
+    let map = L.map('map').setView([20, 10], 3);
+    
+    //https://leaflet-extras.github.io/leaflet-providers/preview/
+
+    let Esri_WorldTerrain = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Terrain_Base/MapServer/tile/{z}/{y}/{x}', {
+        attribution: 'Tiles &copy; Esri &mdash; Source: USGS, Esri, TANA, DeLorme, and NPS',
+        maxZoom: 13
+    });
+    let  Esri_OceanBasemap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Ocean/World_Ocean_Base/MapServer/tile/{z}/{y}/{x}', {
+        attribution: 'Tiles &copy; Esri &mdash; Sources: GEBCO, NOAA, CHS, OSU, UNH, CSUMB, National Geographic, DeLorme, NAVTEQ, and Esri',
+        maxZoom: 13
+    });
+    
+    //Esri_OceanBasemap.addTo(map);
+    Esri_WorldTerrain.addTo(map);
 }
