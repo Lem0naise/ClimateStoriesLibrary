012ed66f09f806f5fbe033fe2a6e462aec864394
Zac Nolan <zac1i1nolan@gmail.com>
2024-07-24 20:29:57 +0100

    added map toggle

--- server/public/script.js
+++ server/public/script.js
@@ -3,23 +3,31 @@ window.onload = function(){
     loadMap();
 }
 
-
+let MAP = false;
 async function loadMap(){ // loading from leaflet js
-    //let map = L.map('map').setView([20, 10], 3);
-    
-    //https://leaflet-extras.github.io/leaflet-providers/preview/
+    if (MAP){
+        let map = L.map('map').setView([20, 10], 3);
+        map.options.maxZoom=9;
 
-    let Esri_WorldTerrain = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Terrain_Base/MapServer/tile/{z}/{y}/{x}', {
-        attribution: 'Tiles &copy; Esri &mdash; Source: USGS, Esri, TANA, DeLorme, and NPS',
-        maxZoom: 13
-    });
-    let  Esri_OceanBasemap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Ocean/World_Ocean_Base/MapServer/tile/{z}/{y}/{x}', {
-        attribution: 'Tiles &copy; Esri &mdash; Sources: GEBCO, NOAA, CHS, OSU, UNH, CSUMB, National Geographic, DeLorme, NAVTEQ, and Esri',
-        maxZoom: 13
-    });
-    return 1;
-    //Esri_OceanBasemap.addTo(map);
-    //Esri_WorldTerrain.addTo(map);
+        //https://leaflet-extras.github.io/leaflet-providers/preview/
+
+        let Esri_WorldTerrain = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Terrain_Base/MapServer/tile/{z}/{y}/{x}', {
+            attribution: 'Tiles &copy; Esri &mdash; Source: USGS, Esri, TANA, DeLorme, and NPS',
+            maxZoom: 13
+        });
+        let Esri_OceanBasemap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Ocean/World_Ocean_Base/MapServer/tile/{z}/{y}/{x}', {
+            attribution: 'Tiles &copy; Esri &mdash; Sources: GEBCO, NOAA, CHS, OSU, UNH, CSUMB, National Geographic, DeLorme, NAVTEQ, and Esri',
+            maxZoom: 9
+        });
+        //Esri_OceanBasemap.addTo(map);
+        Esri_WorldTerrain.addTo(map);
+        return 1;
+    }
+    else {
+        document.getElementById("hidden").style.display = "flex";
+        return 0;
+    }
+    
 }
 
 async function share(){
