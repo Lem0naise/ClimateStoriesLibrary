9eea790268fe660a984e9b935d9fe53bc05c5cb8
Indigo Nolan <indigogonolan@gmail.com>
2025-07-04 17:17:12 +0100

    hotfix: improve regex to allow youtube studio links or youtube shorts links

--- nextjs/climatestorieslibrary/src/utils/getYoutubeVideoId.ts
+++ nextjs/climatestorieslibrary/src/utils/getYoutubeVideoId.ts
@@ -1,5 +1,5 @@
 export function getYouTubeVideoId(url: string): string | null {
-  const regex = /(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/\s]{11})/;
+  const regex = /(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=|shorts\/)|youtu\.be\/|studio\.youtube\.com\/video\/|studio\.youtube\.com\/watch\/)([^"&?\/\s]{11})/;
   const match = url.match(regex);
   return match ? match[1] : null;
 }
